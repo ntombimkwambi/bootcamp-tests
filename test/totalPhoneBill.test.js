40f2743e618c2e50aafc257a0620df0755f6323f
thabiso22 <thabiso@projevtcodex.co>
2018-04-10 15:49:49 +0200

    changes

--- test/totalPhoneBill.test.js
+++ test/totalPhoneBill.test.js
@@ -1,11 +1,11 @@
 describe('totalPhoneBill' , function(){
-  it('should check the amount used for calls' , function(){
-      assert.equal(totalPhoneBill('calls'), 'R2.75' );
+  it('should check the amount used for call' , function(){
+      assert.equal(totalPhoneBill('call'), 'R2.75' );
   });
   it('should check the amount used for sms' , function(){
       assert.equal(totalPhoneBill('sms'), 'R0.65');
   });
   it('should check the total cost of calls and sms' , function(){
-      assert.equal(totalPhoneBill('totalcost'),'7.45' );
+      assert.equal(totalPhoneBill('call, sms, call, sms, sms'),'R7.45' );
   });
 });
